41532a4d7a32b8a5bca616839256c332373a2b3b
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-07-07 12:01:38 -0400

    regex lab

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,19 @@
-def starts_with_a_vowel?(word); end
+def starts_with_a_vowel?(word)
+  word.match?(/\A[aeiou]/i)
+end
 
-def words_starting_with_un_and_ending_with_ing(text); end
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun\w*ing\b/i)
+end
 
-def words_five_letters_long(text); end
+def words_five_letters_long(text)
+  text.scan(/\b[a-zA-Z]{5}\b/)
+end
 
-def first_word_capitalized_and_ends_with_punctuation?(text); end
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match?(/\A[A-Z].*[[:punct:]]\z/)
+end
 
-def valid_phone_number?(phone); end
+def valid_phone_number?(phone)
+  phone.match?(/\d{3}\D*\d{3}\D*\d{4}/)
+end
